618582327b990192de6958fdc751435d7ca1cdef
Simon Svetec <simon.svetec@gmail.com>
2025-01-30 23:14:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+to bo samo služil kot framework
+
+za to različico bom uporabil navigation drawer
+
+Ne pozabi posodobit package name, ko narediš nov fork!!!
+
+
+dodal navigation dependancies
---
+++ app/src/main/java/com/example/gachaframework/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.gachaframework;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.gachaframework.fragment.CounterFragment;
+import com.example.gachaframework.fragment.GamesFragment;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CounterFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_counter);
+            toolbar.setTitle(R.string.counter);
+        }
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                int id = item.getItemId();
+
+                if (id == R.id.nav_counter){
+                    toolbar.setTitle(R.string.counter);
+                    transaction.replace(R.id.fragment_container, new CounterFragment());
+                    transaction.commit();
+                }
+                else if (id == R.id.nav_games){
+                    toolbar.setTitle(R.string.games);
+                    transaction.replace(R.id.fragment_container, new GamesFragment());
+                    transaction.commit();
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
