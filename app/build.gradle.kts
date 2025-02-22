79117e9a5bfe3ec9b0593824e166508449561f7f
Simon Svetec <simon.svetec@gmail.com>
2025-02-22 20:09:06 +0100

    update target sdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "com.sesvete.gachaframework"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
