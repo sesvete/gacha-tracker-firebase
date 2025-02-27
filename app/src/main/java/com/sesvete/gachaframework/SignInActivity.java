69a4f66302ece09d9e8a69865f644b0f8156b6d5
Simon Svetec <simon.svetec@gmail.com>
2025-02-27 20:07:15 +0100

    add sign in intent

--- app/src/main/java/com/sesvete/gachaframework/SignInActivity.java
+++ app/src/main/java/com/sesvete/gachaframework/SignInActivity.java
@@ -1,16 +1,32 @@
 package com.sesvete.gachaframework;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+
 
 public class SignInActivity extends AppCompatActivity {
+
+    private MaterialButton btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
