b104bd99492890fd1d9234352af4f4d273900a47
Simon Svetec <simon.svetec@gmail.com>
2025-03-04 15:53:35 +0100

    fix settings alertDialogWindow

--- app/src/main/java/com/sesvete/gachaframework/fragment/SettingsFragment.java
+++ app/src/main/java/com/sesvete/gachaframework/fragment/SettingsFragment.java
@@ -3,8 +3,11 @@ package com.sesvete.gachaframework.fragment;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 
@@ -49,4 +52,48 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
             mainActivity.updateNavHeader(mainActivity.findViewById(R.id.nav_view));
         }
     }
+
+    // custom alert dialog builder za settingse
+    @Override
+    public void onDisplayPreferenceDialog(@NonNull Preference preference) {
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            AlertDialog.Builder builder = new AlertDialog.Builder(requireContext(), R.style.RoundedDialog); // Apply your theme
+
+            // za sestavo radio buttona
+            // list Preference.get entries je char sequence zato moramo njapre to pretvorit v String []
+
+            CharSequence[] entriesCharSequence = listPreference.getEntries();
+            CharSequence[] entryValuesCharSequence = listPreference.getEntryValues();
+
+            String[] entries = new String[entriesCharSequence.length];
+            String[] entryValues = new String[entryValuesCharSequence.length];
+
+            for (int i = 0; i < entriesCharSequence.length; i++) {
+                entries[i] = entriesCharSequence[i].toString();
+                entryValues[i] = entryValuesCharSequence[i].toString();
+            }
+
+            String currentValue = listPreference.getValue();
+
+            int checkedItem = -1; // default - ni izbrane možnosti
+            for (int i = 0; i < entryValues.length; i++) {
+                if (entryValues[i].equals(currentValue)) {
+                    checkedItem = i;
+                    break;
+                }
+            }
+            // dejansko sestavi window
+            builder.setTitle(listPreference.getTitle())
+                    .setSingleChoiceItems(entries, checkedItem, (dialog, which) -> {
+                        listPreference.setValue(entryValues[which]);
+                        dialog.dismiss();
+                    })
+                    .setNegativeButton(R.string.cancel, null)
+                    .show();
+        } else {
+            super.onDisplayPreferenceDialog(preference); // Use the default dialog for other preferences
+        }
+    }
+
 }
