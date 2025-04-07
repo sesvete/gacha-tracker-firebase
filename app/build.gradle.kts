62f772986c5b3727023e9dbe5102a10570368dca
Simon Svetec <simon.svetec@gmail.com>
2025-04-07 15:22:55 +0200

    upgrade to api 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.sesvete.gachatrackerfirebase"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.sesvete.gachatrackerfirebase"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
