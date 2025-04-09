4f8bdf2248ab260b03cf3cb5bacf24890ee73afd
Simon Svetec <simon.svetec@gmail.com>
2025-04-09 12:43:33 +0200

    add sign in with username layout

--- app/src/main/java/com/sesvete/gachatrackerfirebase/SignInActivity.java
+++ app/src/main/java/com/sesvete/gachatrackerfirebase/SignInActivity.java
@@ -1,6 +1,7 @@
 package com.sesvete.gachatrackerfirebase;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -23,7 +24,8 @@ import com.sesvete.gachatrackerfirebase.helper.LocaleHelper;
 
 public class SignInActivity extends AppCompatActivity {
 
-    private MaterialButton btnSignIn;
+    private MaterialButton btnSignInWithGoogle;
+    private MaterialButton btnSignInWithUsername;
     private FirebaseAuth mAuth;
     private CredentialManager credentialManager;
 
@@ -44,9 +46,22 @@ public class SignInActivity extends AppCompatActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.language_fragment_container, LanguageSettingsFragment.class, null).setReorderingAllowed(true).commit();
 
-        btnSignIn = findViewById(R.id.btn_sign_in_google);
+        btnSignInWithUsername = findViewById(R.id.btn_sign_in_password);
+        // basically se bo to delalo na novem activity
 
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
+        btnSignInWithGoogle = findViewById(R.id.btn_sign_in_google);
+
+
+        btnSignInWithUsername.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this, SignInWithPasswordActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnSignInWithGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AuthenticationHelper.launchCredentialManager(getResources(), credentialManager, mAuth, SignInActivity.this);
---
+++ app/src/main/java/com/sesvete/gachatrackerfirebase/SignInWithPasswordActivity.java
@@ -0,0 +1,51 @@
+package com.sesvete.gachatrackerfirebase;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+import com.sesvete.gachatrackerfirebase.helper.DialogHelper;
+import com.sesvete.gachatrackerfirebase.helper.LocaleHelper;
+
+public class SignInWithPasswordActivity extends AppCompatActivity {
+
+    private EditText editTextSignInPasswordUsername;
+    private EditText editTextSignInPasswordPassword;
+    private MaterialButton btnSignInPasswordSignIn;
+    private MaterialButton btnSignInPasswordNewUser;
+    private MaterialButton btnSignInPasswordBack;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(LocaleHelper.onAttach(newBase));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_with_password);
+
+        editTextSignInPasswordUsername = findViewById(R.id.edit_text_sign_in_password_username);
+        editTextSignInPasswordPassword = findViewById(R.id.edit_text_sign_in_password_password);
+        btnSignInPasswordSignIn = findViewById(R.id.btn_sign_in_password_sign_in);
+        btnSignInPasswordNewUser = findViewById(R.id.btn_sign_in_password_new_user);
+
+        btnSignInPasswordBack = findViewById(R.id.btn_sign_in_password_back);
+
+        DialogHelper.keyboardTouchListener(findViewById(R.id.main_sign_in_with_password), this);
+
+        btnSignInPasswordBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInWithPasswordActivity.this, SignInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
